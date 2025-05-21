c3355ca9bb742cb97e309a3fa74c602d1e826ff5
Jonathan Mesquita <81002250+jonathanmesquita@users.noreply.github.com>
2025-05-21 18:41:27 -0300

    Update leitor.js

--- leitor.js
+++ leitor.js
@@ -4,8 +4,14 @@ const csv = require('csv-parser');
 const resultados = [];
 
 fs.createReadStream('arquivo.csv', { encoding: 'utf8' })
-  .pipe(csv({ separator: ';' }))
-  .on('data', (data) => resultados.push(data))
+  .pipe(csv({ separator: ';' }))  // Altere o separador se necessário
+  .on('data', (data) => {
+    resultados.push(data);
+  })
   .on('end', () => {
-    console.log(resultados);
+    console.log('Leitura completa');
+    console.log(resultados);  // Exibe os dados lidos no console
+  })
+  .on('error', (err) => {
+    console.error('Erro na leitura do arquivo:', err);
   });
